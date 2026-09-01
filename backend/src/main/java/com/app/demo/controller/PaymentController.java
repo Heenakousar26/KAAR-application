021c6334a5471c2b179081672657dde56c266430
KOLIMI HEENA KOUSAR <heenakousar12354@gmail.com>
2026-09-01 21:50:38 +0530

    Update CORS configuration in PaymentController

--- backend/src/main/java/com/app/demo/controller/PaymentController.java
+++ backend/src/main/java/com/app/demo/controller/PaymentController.java
@@ -8,10 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@CrossOrigin(origins = {
-        "http://localhost:5173",
-        "https://frontend-ashy-phi-78.vercel.app"
-})
+@CrossOrigin(origins = "*")
 @RestController
 public class PaymentController {
 
